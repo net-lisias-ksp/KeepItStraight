ea44c2bf6f38d0619656d527b2190833b91976d7
Real-Gecko <alexandrbezenkov@gmail.com>
2016-10-13 02:15:00 +0600

    Version 1.0.1

--- KeepItStraight/KeepItStraight.cs
+++ KeepItStraight/KeepItStraight.cs
@@ -7,25 +7,39 @@ namespace KeepItStraight
 	[KSPAddon(KSPAddon.Startup.Flight, false)]
 	public class KeepItStraight: MonoBehaviour
 	{
+		public static KeepItStraight instance;
 		private PluginConfiguration config;
 		FlightCamera camera;
+
 		void Awake() {
+			if (instance != null) {
+				Destroy (this);
+				return;
+			}
+
 			if (config == null) {
 				config = PluginConfiguration.CreateForType<KeepItStraight> ();
 			}
 			config.load ();
 			camera = FlightCamera.fetch;
 			GameEvents.onFlightReady.Add (FlightReady);
+			GameEvents.onPartCouple.Add (onPartCouple);
 		}
 
 		void OnDestroy()
 		{
 			GameEvents.onFlightReady.Remove (FlightReady);
+			GameEvents.onPartCouple.Remove (onPartCouple);
 			config.SetValue ("CameraMode", FlightCamera.CamMode);
 			config.save ();
 		}
 
 		void FlightReady() {
+			int CamMode = config.GetValue<int>("CameraMode");
+			camera.setModeImmediate((FlightCamera.Modes)CamMode);
+		}
+
+		void onPartCouple(GameEvents.FromToAction<Part,Part> action) {
 			int CamMode = config.GetValue<int>("CameraMode");
 			camera.setModeImmediate((FlightCamera.Modes)CamMode);
 		}
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # KSP-KeepItStraight
 
 Remembers camera mode between scene switches so you will not end up with AUTO every time.
+
+# Changelog
+## Version 1.0.1
+- Camera mode is not reset to AUTO after docking is complete
